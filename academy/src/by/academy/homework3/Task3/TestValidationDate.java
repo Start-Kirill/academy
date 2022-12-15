4c884b3315d2079a61bf6ece8b60ddb3ac195b81
Ks <Ks@DESKTOP-DDV8GI3>
2022-12-15 21:23:05 +0300

    Homework #3

---
+++ academy/src/by/academy/homework3/Task3/TestValidationDate.java
@@ -0,0 +1,12 @@
+package by.academy.homework3.Task3;
+
+public class TestValidationDate {
+
+	public static void main(String[] args) {
+
+		ValidationDate validator = new ValidationDate();
+
+		System.out.println(validator.validate("29-02-1704"));
+
+	}
+}
